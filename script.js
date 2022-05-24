a11704299a0f3b8cd4134c9fde71169be2d6132b
bigmosi <kinyeramo@gmail.com>
2022-05-24 11:58:09 +0300

    Add new book on the page

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ const myForm = document.getElementById('form');
 const bookList = document.getElementById('book-list');
 newDiv.classList.add('mylist');
 bookList.appendChild(newDiv);
+
+const newBook = (title, author) => {
+    const data = {
+        bookTitle: title,
+        bookAuthor: author,
+    };
+    bookData.push(data);
+    localStorage.setItem('book', JSON.stringify(bookData));
+    newDiv.innerHTML += `<div>
+          <p><strong>${data.bookTitle}</strong></p>
+          <p><strong>${data.bookAuthor}</strong></p>
+          <button class="remove">delete</button>
+          <hr/>
+          </div>`;
+    myForm.reset();
+};
