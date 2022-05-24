f5036212e144c9f9fe85cd0529762cbf3b45150b
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-05-24 10:03:53 +0100

    add removeBook and getDataFromStore functions

--- script.js
+++ script.js
@@ -26,3 +26,35 @@ const newBook = (title, author) => {
           </div>`;
     myForm.reset();
 };
+
+// Remove book from store function
+const removeBook = () => {
+  newDiv.addEventListener('click', (e) => {
+    if (e.target.classList.contains('remove')) {
+      const list = e.target.parentElement;
+      const bookTitle = list.childNodes[4].value;
+      const remain = bookData.filter((book) => book.bookTitle !== bookTitle);
+      localStorage.setItem('book', JSON.stringify(remain));
+      newDiv.removeChild(list);
+    }
+  });
+};
+
+// Function to get data from local storage
+const getDataFromStore = () => {
+  window.addEventListener('load', () => {
+    if (localStorage.getItem('book')) {
+      const books = JSON.parse(localStorage.getItem('book'));
+      books.forEach((data) => {
+        newDiv.innerHTML += `<div>
+              <p><strong>${data.bookTitle}</strong></p>
+              <p><strong>${data.bookAuthor}</strong></p>
+              <button class="remove">delete</button>
+              <hr/>
+              </div>`;
+        bookData.push(books);
+      });
+    }
+  });
+};
+
